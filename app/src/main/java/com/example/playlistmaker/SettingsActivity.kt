9a28de8deaa63599acd66be8e0c371807c236b2d
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-09-10 18:26:19 +0300

    Third homework: added intents

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+        val back = findViewById<ImageView>(R.id.back_button)
+        back.setOnClickListener{
+            val menuIntent = Intent(this, MainActivity::class.java)
+            startActivity(menuIntent)
+        }
+    }
+}
