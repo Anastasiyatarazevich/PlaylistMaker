0d7811e341676a016a444d20ae9713abf8c414c7
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-09-19 23:50:27 +0300

    Sprint 9: added intents

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -14,8 +16,51 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val back = findViewById<ImageView>(R.id.back_button)
-        back.setOnClickListener{
+        val shareAppButton = findViewById<FrameLayout>(R.id.share_app)
+        val writeSupportButton = findViewById<FrameLayout>(R.id.support)
+        val agreementButton = findViewById<FrameLayout>(R.id.user_agreement)
+
+        back.setOnClickListener {
             finish()
+        }
+
+        shareAppButton.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.share_text)
+                )
+            }
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)))
+        }
+
+        writeSupportButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:${getString(R.string.support_email)}")
+                putExtra(
+                    Intent.EXTRA_EMAIL,
+                    getString(R.string.support_email))
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    getString(R.string.support_subject)
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.support_message)
+                )
+            }
+            if (emailIntent.resolveActivity(packageManager) != null) {
+                startActivity(emailIntent)
+            }else
+                Toast.makeText(this, getString(R.string.share_text_fail), Toast.LENGTH_SHORT).show()
+        }
+
+        agreementButton.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_url)))
+            startActivity(browserIntent)
         }
     }
 }
