0f71e7f3d64169955a7288b93e939b0be45087d7
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-09-20 23:19:14 +0300

    Sprint 9: added saved data

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchText: String? = null
+
+    companion object {
+        private const val SEARCH_TEXT_KEY = "search_text_key"
+    }
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,25 +27,44 @@ class SearchActivity : AppCompatActivity() {
         val searchInput = findViewById<EditText>(R.id.search_input)
         val clearButton = findViewById<ImageView>(R.id.clear_button)
 
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT_KEY)
+            searchInput.setText(searchText)
+        }
+
         back.setOnClickListener {
             finish()
         }
 
         clearButton.setOnClickListener {
             searchInput.setText("")
+            hideKeyboard()
         }
 
         searchInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = s.toString()
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
-            override fun afterTextChanged(s: Editable?) {
-            }
+            override fun afterTextChanged(s: Editable?) {}
         })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val searchText = savedInstanceState.getString(SEARCH_TEXT_KEY)
+        val searchInput: EditText? = findViewById(R.id.search_input)
+        searchInput?.setText(searchText)
     }
 
     fun clearButtonVisibility(s: CharSequence?): Int {
@@ -48,4 +75,11 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+    fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        val searchInput: EditText? = findViewById(R.id.search_input)
+        if (searchInput != null) {
+            imm.hideSoftInputFromWindow(searchInput.windowToken, 0)
+        }
+    }
 }
