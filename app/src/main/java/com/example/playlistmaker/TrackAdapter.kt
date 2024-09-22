3af3f1e7d7f304c48e1a4ba472093eed9e08a056
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-09-22 22:29:42 +0300

    Sprint 10: added improvements

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,45 +1,10 @@
 package com.example.playlistmaker
 
-import android.content.Context
-import android.util.TypedValue
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackAdapter(private val trackList: List<Track>) : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
 
-    class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
-    ) {
-        private val trackNameTextView: TextView = itemView.findViewById(R.id.track_name)
-        private val artistNameTextView: TextView = itemView.findViewById(R.id.artist_name)
-        private val trackTimeTextView: TextView = itemView.findViewById(R.id.track_time)
-        private val artworkImageView: ImageView = itemView.findViewById(R.id.artwork_image)
-
-        fun bind(track: Track) {
-            trackNameTextView.text = track.trackName
-            artistNameTextView.text = track.artistName
-            trackTimeTextView.text = track.trackTime
-
-            Glide.with(itemView)
-                .load(track.artworkUrl100)
-                .centerCrop()
-                .placeholder(R.drawable.place)
-                .transform(RoundedCorners(dpToPx(2f, itemView.context)))
-                .into(artworkImageView)
-        }
-        fun dpToPx(dp: Float, context: Context): Int {
-            return TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dp,
-                context.resources.displayMetrics).toInt()
-        }
-    }
-
+class TrackAdapter(private val trackList: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         return TrackViewHolder(parent)
     }
@@ -51,5 +16,4 @@ class TrackAdapter(private val trackList: List<Track>) : RecyclerView.Adapter<Tr
     override fun getItemCount(): Int {
         return trackList.size
     }
-
 }
